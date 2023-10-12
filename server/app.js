286a8955de6040477ec01d5e1ecdc37ecb5a6376
5thaks07 <5thaks07@solent.ac.uk>
2023-10-12 11:43:54 +0100

    database connection , added network to the build script

--- server/app.js
+++ server/app.js
@@ -1,6 +1,22 @@
+import {} from 'dotenv/config'
 import express from 'express';
+import chalk  from 'chalk';
+import mongoose from 'mongoose';
+
 
 const app = express();
+
+const { MONGODB_URI } = process.env;
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+  console.log(
+    "MongoDB connection error. Please make sure MongoDB is running.",
+    chalk.red("✗")
+  );
+  process.exit();
+});
 
 app.use(express.static('public'));
 
