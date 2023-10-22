5476c2f4ac060f861d10181a2874aeea03f1d926
Joao-Potes <4fragj68@solent.ac.uk>
2023-10-22 10:08:35 +0100

    Login page created

--- server/app.js
+++ server/app.js
@@ -3,11 +3,16 @@ import express from 'express';
 import chalk from 'chalk';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
-
+import ejs from 'ejs';
 import UserRouter from './routes/user.route.js';
 import PoiRouter from './routes/poi.route.js';
+import { fileURLToPath } from 'url'; // Import fileURLToPath
+import path from 'path'; // Import the 'path' module
 
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url); // Get the filename of the current module
+const __dirname = path.dirname(__filename); // Get the directory name
 
 const { MONGODB_URI } = process.env;
 
@@ -40,6 +45,53 @@ app.get('/', (req, res, next) => {
 app.use('/user', UserRouter);
 
 app.use('/pois', PoiRouter);
+
+app.get('/home', function (req, res) {
+    res.render('home');
+});
+
+app.set('view engine', 'ejs');
+
+app.set('views', path.join(__dirname, 'views'));
+
+// Showing register form
+app.get('/register', function (req, res) {
+    res.render('register');
+});
+
+app.post('/register', async (req, res) => {
+    const user = await User.create({
+        username: req.body.username,
+        password: req.body.password,
+    });
+
+    return res.status(200).json(user);
+});
+
+//Showing login form
+app.get('/login', function (req, res) {
+    res.render('login');
+});
+
+app.post('/login', async function (req, res) {
+    try {
+        // check if the user exists
+        const user = await User.findOne({ username: req.body.username });
+        if (user) {
+            //check if password matches
+            const result = req.body.password === user.password;
+            if (result) {
+                res.render('secret');
+            } else {
+                res.status(400).json({ error: "password doesn't match" });
+            }
+        } else {
+            res.status(400).json({ error: "User doesn't exist" });
+        }
+    } catch (error) {
+        res.status(400).json({ error });
+    }
+});
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
