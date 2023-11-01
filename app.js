09ac9073e3c87bc4da9219cde895be7ee7af36ab
Faizan Ahmed <5ahmef94@solent.ac.uk>
2023-11-01 13:02:46 +0000

    Prevented the app from listening to requests on port 3000 when the tests are being run.

--- app.js
+++ app.js
@@ -39,8 +39,10 @@ app.use('/pois', PoiRouter);
 
 app.use('/healthcheck', HealthRouter);
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+if (process.env.NODE_ENV !== 'test') {
+    app.listen(3000, () => {
+        console.log('Server is running on port 3000');
+    });
+}
 
 export default app;
