6f1b851a7f1a02a6bfd6454ae4202765ce35d4db
5thaks07 <5thaks07@solent.ac.uk>
2023-11-02 15:06:49 +0000

    changes in  login.js

--- public/login.js
+++ public/login.js
@@ -3,6 +3,7 @@ Login();
 async function Login() {
     document.getElementById('login').addEventListener('click', async (e) => {
         e.preventDefault
+       
         const user = {
             email: document.getElementById('email').value,
             password: document.getElementById('password').value,
@@ -15,19 +16,16 @@ async function Login() {
                 },
                 body: JSON.stringify(user),
             });
-            if (response.status == 200) {
-                alert('Login Successful')
-                redirect();
-            } else if (response.status == 401) {
-                alert('INCORRECT LOGIN');
-            } else {
-                alert(`Unknown error: code ${response.status}`);
-            }
+
+            if (response.status === 401) {
+                return alert('Incorrect Login')
+            } else if (response.status !== 200) {
+               return alert(`Unknown error: code ${response.status}`);
+            } 
         } catch (error) {
-            alert(`Error: ${error}`);
+           return alert(`Error: ${error}`);
         }
+        alert('Login Successful');
+        return (window.location.href = '/index.html');
     });
-}
-function redirect(){
-    location.replace("index.html")
 }
