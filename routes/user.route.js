5a0d7ae860a54ed7923e59f5178bb4fca8ed6da1
5thaks07 <5thaks07@solent.ac.uk>
2023-11-05 19:43:22 +0000

    changes

--- routes/user.route.js
+++ routes/user.route.js
@@ -6,7 +6,6 @@ import * as UserController from '../controllers/user.controller.js';
 
 const router = Router();
 
-
 router.post('/login', UserController.login);
 router.post('/register', UserController.create);
 
@@ -14,4 +13,5 @@ router.put('/updateUser', authMiddleware, UserController.updateUser);
 router.get('/profile', authMiddleware, UserController.profile);
 router.get('/list', authMiddleware, UserController.getUser);
 router.get('/logout', authMiddleware, UserController.logout);
+
 export default router;
