ea81c9fb117ecf695938c43999ba6e2546b62cf1
TomStrong <5strot78@solent.ac.uk>
2023-11-07 14:28:45 +0000

    Updated POI API endpoints to allow ownership

--- routes/poi.route.js
+++ routes/poi.route.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import * as PoiController from '../controllers/poi.controller.js';
+import { stdAuth } from '../middlewares/user.middleware.js';
 
 const router = Router();
 
-router.get('/list', PoiController.getPois);
-router.post('/addPoi', PoiController.addPoi);
-router.delete('/deletePoi/:id', PoiController.deletePoi);
-router.put('/updatePoi', PoiController.updatePoi);
+router.get('/list', stdAuth, PoiController.getPois);
+router.get('/:id', stdAuth, PoiController.getPoiById);
+router.post('/addPoi', stdAuth, PoiController.addPoi);
+router.delete('/deletePoi/:id', stdAuth, PoiController.deletePoi);
+router.put('/updatePoi/:id', stdAuth, PoiController.updatePoi);
 
 export default router;
