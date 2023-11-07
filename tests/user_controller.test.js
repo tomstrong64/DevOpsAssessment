f2360cecf6d506744af2dbce2f57ce97994c03d2
Faizan Ahmed <5ahmef94@solent.ac.uk>
2023-11-07 13:24:23 +0000

    Some tests written, some work but others need some changes.

--- tests/user_controller.test.js
+++ tests/user_controller.test.js
@@ -12,7 +12,30 @@ describe('POST /register', () => {
                 "password": 'jumjams1234',
             })
             .set('Content-Type', 'application/json');
-        expect(response.headers['Content-Type']).toBeUndefined();
-        expect(response.status).toBe(201);
-    }, 10000);
+        expect(response.status).toBe(201);  // returns 302, which is known as 'Found', it means that the URI of the requested URI has been changed temporarily
+    }, 60000);
+
+    it('the headers should be defined', async() => {
+        const response = await request(app)
+            .post('/user/register')
+            .send({
+                "name": "Test Name 2",
+                "email": "testingmail11@yahoo.com",
+                "password": "adbdfec2891",
+            })
+            .set('Content-Type', 'application/json');
+            expect(response.headers).toBeDefined();
+    }, 60000);
+
+    it("the response code should be appropriate for this request' ", async() => {
+        const response = await request(app)
+        .post('/user/register')
+        .send({
+            "name": "DUMMY_Alpha",
+            "email": "dum273@gmail.com",
+            "password": "luo2ry92@a1h",
+        })
+        .set('Content-Type', 'application/json')
+        expect(response.statusCode).toBe(200);
+    }, 60000)
 });
