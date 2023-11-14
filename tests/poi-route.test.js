46a16098f48b3affa75b1bcf584a4db805d8a1c5
Faizan Ahmed <5ahmef94@solent.ac.uk>
2023-11-14 12:44:30 +0000

    Added another missing dependency.

--- tests/poi-route.test.js
+++ tests/poi-route.test.js
@@ -1,6 +1,7 @@
 import app from '../app.js';
 import request from 'supertest';
 import mongoose from 'mongoose';
+import chalk from 'chalk';
 
 let auth_token; //Private authorisation stuff, should not be exposed outside!
 
