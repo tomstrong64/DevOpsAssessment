daf10606b43db0bbb19d8f5f12154ff0d2763573
ali1373107 <alidashti1994a@gmail.com>
2023-11-14 13:34:47 +0000

    successfully getting poi id from response

--- tests/routeAddPoi.test.js
+++ tests/routeAddPoi.test.js
@@ -1,43 +1,59 @@
 import app from '../app.js';
 import request from 'supertest';
-let poi_region = 'London'
+
+let poi_region = 'London';
+let poi_id;
+
 describe('POST /pois/addPoi', () => {
     it('should add new POI', async () => {
-        const response = await request(app).post('/pois/addPoi').send({
-            "name" :"Swindon",
-            "type": "Test Type",
-            "country": poi_region,
-            "region": 'Solent',
-            "lat": 50.9105,
-            "lon": -1.4049,
-            "description": "Test Description"
-
-        })
-        .set('Content-Type','application/json')
-        .set('Authorization','Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1NGNjNTRjMjI0NTRhY2QyNTEyNTZlMyIsImlhdCI6MTY5OTk1OTk1NCwiZXhwIjoxNzAwMDQ2MzU0fQ.jfzCKYEK9n3-i4jvb7a_ODPazXT-dusLPanxnF6xEpk');
+        const response = await request(app)
+            .post('/pois/addPoi')
+            .send({
+                name: 'Swindon',
+                type: 'Test Type',
+                country: poi_region,
+                region: 'Solent',
+                lat: 50.9105,
+                lon: -1.4049,
+                description: 'Test Description',
+            })
+            .set('Content-Type', 'application/json')
+            .set(
+                'Authorization',
+                'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1NGNjNTRjMjI0NTRhY2QyNTEyNTZlMyIsImlhdCI6MTY5OTk1OTk1NCwiZXhwIjoxNzAwMDQ2MzU0fQ.jfzCKYEK9n3-i4jvb7a_ODPazXT-dusLPanxnF6xEpk'
+            );
+        poi_id = response.body._id;
+        console.log(poi_id);
         expect(response.status).toEqual(201);
-    },);
+    });
 });
-describe('GET /pois/list?search=poi_region', () => {
-  
-  it('should return POIs by region name', async () => {
-   const response = await request(app).get(`/pois/list?search=${poi_region}`)
-    .set('Authorization','Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1NGNjNTRjMjI0NTRhY2QyNTEyNTZlMyIsImlhdCI6MTY5OTk1OTk1NCwiZXhwIjoxNzAwMDQ2MzU0fQ.jfzCKYEK9n3-i4jvb7a_ODPazXT-dusLPanxnF6xEpk');
 
-    expect(response.status).toEqual(200);
-  });
+describe('GET /pois/list?search=poi_region', () => {
+    it('should return POIs by region name', async () => {
+        const response = await request(app)
+            .get(`/pois/list?search=${poi_region}`)
+            .set(
+                'Authorization',
+                'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1NGNjNTRjMjI0NTRhY2QyNTEyNTZlMyIsImlhdCI6MTY5OTk1OTk1NCwiZXhwIjoxNzAwMDQ2MzU0fQ.jfzCKYEK9n3-i4jvb7a_ODPazXT-dusLPanxnF6xEpk'
+            );
+
+        expect(response.status).toEqual(200);
+    });
 });
 
 it('should return all POIs when no query parameters are provided', async () => {
-  const response = await request(app).get('/pois/list')
-  .set('Authorization','Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1NGNjNTRjMjI0NTRhY2QyNTEyNTZlMyIsImlhdCI6MTY5OTk1OTk1NCwiZXhwIjoxNzAwMDQ2MzU0fQ.jfzCKYEK9n3-i4jvb7a_ODPazXT-dusLPanxnF6xEpk');
+    const response = await request(app)
+        .get('/pois/list')
+        .set(
+            'Authorization',
+            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1NGNjNTRjMjI0NTRhY2QyNTEyNTZlMyIsImlhdCI6MTY5OTk1OTk1NCwiZXhwIjoxNzAwMDQ2MzU0fQ.jfzCKYEK9n3-i4jvb7a_ODPazXT-dusLPanxnF6xEpk'
+        );
 
-  expect(response.status).toEqual(200);
-  expect(typeof response.body).toBe('object');
+    expect(response.status).toEqual(200);
+    expect(typeof response.body).toBe('object');
 });
 
-
-  /*it('should return a single POI by ID', async () => {
+/*it('should return a single POI by ID', async () => {
    
     const response = await request(app).get(`/pois/getPois?id=${poiId}`)
         .set('Authorization','Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1NGNjNTRjMjI0NTRhY2QyNTEyNTZlMyIsImlhdCI6MTY5OTYzNTU4MiwiZXhwIjoxNjk5NzIxOTgyfQ.7yv4NlcBbzvI62d2awQjhrkcm6GBSnRENiwrameMSjM');
@@ -69,4 +85,3 @@ it('should return all POIs when no query parameters are provided', async () => {
   
 });
 */
-  
