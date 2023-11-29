626b9da5720ce63949dd2cbeed167e0fc1cc191d
Joao-Potes <4fragj68@solent.ac.uk>
2023-11-29 13:22:31 +0000

    fixed path for locales , having working example

--- i18n.js
+++ i18n.js
@@ -1,8 +1,13 @@
 import i18n from 'i18n';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 i18n.configure({
     locales: ['en', 'pt'],
     defaultLocale: 'en',
+    directory: path.join(__dirname, 'locales'),
     cookie: 'i18n',
 });
 
